24e9446f04b39723d5bec50442b5aea86d30d6d6
Noyal Jacob <johndoe@example.comNoyal>
2021-07-13 14:03:15 -0500

    count by 2 and hide tracks

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = 'seceret key'
+
+@app.route('/')
+def counter():
+    if 'count' not in session:
+        session['count'] = 1
+    else:
+        session['count'] += 1
+    print(session['count'])
+    return render_template(('index.html'), count = session['count'])
+
+@app.route('/click_me')
+def add_two():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/destroy_session')
+def counter_erases():
+    session.clear()
+    return redirect("/")
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
